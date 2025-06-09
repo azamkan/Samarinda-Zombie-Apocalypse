f3b8749b7c188aaa954b06670c4c7b0bf66ce87e
Abdullah Azam <abdullahazam640@gmail.com>
2025-06-10 01:42:21 +0800

    fix-4

--- Assets/MainMenuScript.cs
+++ Assets/MainMenuScript.cs
@@ -1,8 +1,21 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuScript : MonoBehaviour
 {
+    public GameObject exitPanel;        // Panel peringatan keluar
+    public Button yesButton;            // Tombol YA
+    public Button noButton;             // Tombol TIDAK
+
+    private void Start()
+    {
+        exitPanel.SetActive(false);     // Sembunyikan panel di awal
+
+        yesButton.onClick.AddListener(ExitGame);
+        noButton.onClick.AddListener(ClosePanel);
+    }
+
     public void SelectLevel(string scenename)
     {
         SceneManager.LoadScene(scenename);
@@ -11,4 +24,14 @@ public class MainMenuScript : MonoBehaviour
     {
         Application.Quit();
     }
+    void ClosePanel()
+    {
+        exitPanel.SetActive(false); // Sembunyikan panel
+    }
+
+    public void showpanelexit()
+    {
+        exitPanel.SetActive(true);
+    }
+
 }
